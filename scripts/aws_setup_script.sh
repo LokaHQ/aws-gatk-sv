a9c7fdb5b5d54aaf62eb0d0353f2e69f5fb570a8
Henrique Silva <henrique.rd.silva@gmail.com>
2022-08-12 14:50:23 +0100

    fix some file references

--- scripts/aws_setup_script.sh
+++ scripts/aws_setup_script.sh
@@ -44,7 +44,6 @@ GATK_SV_ROOT=$(pwd)
 CLOUD_ENV="aws.gatk_sv"
 echo '{ "google_project_id": "broad-dsde-methods", "terra_billing_project_id": "broad-dsde-methods" }' > inputs/values/"${CLOUD_ENV}".json
 python3 scripts/inputs/build_inputs.py "${BASE_DIR}"/inputs/values "${BASE_DIR}"/inputs/templates/test "${BASE_DIR}"/inputs/build/ref_panel_1kg/test  -a '{ "test_batch" : "ref_panel_1kg", "cloud_env" : "'$CLOUD_ENV'" }'
-cp inputs/build/ref_panel_1kg/test/GATKSVPipelineBatch/GATKSVPipelineBatch.json
 cp inputs/build/ref_panel_1kg/test/GATKSVPipelineBatch/GATKSVPipelineBatch.json gatk_run/
 
 cd gatk_run
@@ -62,9 +61,9 @@ zip dep.zip *.wdl
 # Update and Copy the aws config file.
 cd ../
 # wget https://github.com/lokahq/aws-gatk-sv/blob/master/configs/aws_GATKSVPipelineBatch.json\?raw\=true -O aws_GATKSVPipelineBatch.json
-cp ../configs/aws_GATKSVPipelineBatch.json aws_GATKSVPipelineBatch.json 
+cp ../../configs/aws_GATKSVPipelineBatch.json aws_GATKSVPipelineBatch.json 
 # wget https://github.com/lokahq/aws-gatk-sv/blob/master/configs/opts.json\?raw\=true -O opts.json
-cp ../configs/opts.json opts.json
+cp ../../configs/opts.json opts.json
 chmod 755 aws_GATKSVPipelineBatch.json opts.json
 # Update the aws_GATKSVPipelineBatch.ref_panel_1kg.json json with the correct values as per variables defined
 array=( BROAD_REF_PATH CRAM_BAM_FILE_PATH HAPLOTYPE_GVCF_PATH GATK_SV_RESOURCES_PATH BATCH_DEF_PATH AWS_ACCOUNT_ID AWS_REGION ECR_REPO_NAME )
