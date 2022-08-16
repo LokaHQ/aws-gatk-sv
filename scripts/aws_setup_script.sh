11e2df8fcce96e81806ec22f1ad81b69ec2d4954
Henrique Silva <henrique.rd.silva@gmail.com>
2022-08-16 16:30:05 +0100

    start docker

--- scripts/aws_setup_script.sh
+++ scripts/aws_setup_script.sh
@@ -17,12 +17,13 @@ sudo yum install -y jq docker
 sudo usermod -aG docker ec2-user
 id ec2-user
 newgrp docker
+sudo service docker start
+sudo chkconfig docker on
+
 docker run hello-world
 
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
-# sudo service docker start
-# sudo chkconfig docker on
 # # sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
 # sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
 # sudo chmod +x /usr/local/bin/docker-compose
